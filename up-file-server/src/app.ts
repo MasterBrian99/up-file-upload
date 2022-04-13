bd17df79be14b469cb51aa59ff91e56a84d8da27
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-04-13 19:47:31 +0530

    add file upload

--- up-file-server/src/app.ts
+++ up-file-server/src/app.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
 import morgan from 'morgan';
 const app = express.default();
+
 import { default as fileRouter } from "./controllers/file.controller";
+app.use(express.json())
 
 app.use(morgan('combined'))
 
--- up-file-server/src/controllers/file.controller.ts
+++ up-file-server/src/controllers/file.controller.ts
@@ -1,10 +1,52 @@
 import { Request, Response } from "express";
 import { Router } from "express";
+import multer from 'multer';
+
+const storage = multer.memoryStorage()
+import drive from '../configs/cloudConfig';
+import { Readable } from "stream";
+const upload = multer({ storage: storage });
+
 const router = Router();
 
-router.get("/add", (_req: Request, res: Response) => {
+router.post("/add", upload.single('file'), async (req: Request, res: Response) => {
+    const title = req.body.title;
+    const file = req.file;
+    const buffer = req.file?.buffer;
+    console.log(title);
+    console.log(file);
+    await uploadFile(buffer);
+
     res.send('Hello')
+
 });
 
+async function uploadFile(buffer: Buffer | undefined) {
+    try {
+        const response = await drive.files.create({
+            requestBody: {
+                name: 'hero1.png', //file name
+                mimeType: 'image/png',
+                parents: ['1S2WRQOMvr0WIGJ3shP0eyOXGvR0yvp6n']
+            },
+            media: {
+                mimeType: 'image/png',
+                body: bufferToStream(buffer)
+            },
+        });
+
+        console.log(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function bufferToStream(buffer: any) {
+    var stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+
+    return stream;
+}
 
 export default router;
