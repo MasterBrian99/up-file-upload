cf7cb1350a15be072ada547fff9923bcaf6a7e8e
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-04-14 00:10:38 +0530

    update path

--- up-file-server/src/app.ts
+++ up-file-server/src/app.ts
@@ -1,11 +1,23 @@
 import * as express from "express";
 import morgan from 'morgan';
 const app = express.default();
-
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { default as fileRouter } from "./controllers/file.controller";
 app.use(express.json())
 
 app.use(morgan('combined'))
 
-app.use('/file', fileRouter)
+
+app.use('/api/file', fileRouter)
+app.use('/api/get', createProxyMiddleware({
+    target: 'https://drive.google.com',
+    changeOrigin: true, // needed for virtual hosted sites
+    ws: true, // proxy websockets
+    pathRewrite: {
+        '^/api/get': '', // rewrite path
+    },
+    router: () => {
+        return "https://drive.google.com/uc?id=18OHnxAyv9DPhoapL-6rtAMXUvE8B9sbr&export=download";
+    }
+}))
 export default app;
